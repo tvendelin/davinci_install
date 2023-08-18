6442eb49cb6425125b16af2bf862fee60926c2b3
Tommy Atkins <foo.bar@example.com>
2023-08-18 19:54:08 +0200

    Add packages needed by Davinci Resolve

---
+++ davinci_on_void_install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Nvidia drivers come from here
+xbps-install void-repo-nonfree xtools
+
+# update everything
+xbps-install -Su
+
+xbps-install vim-huge git fzf fd tmux lynx xorg alsa-utils \
+    base-devel libX11-devel libXft-devel libXinerama-devel \
+    google-fonts-ttf \
+    xkblayout-state \
+
+    # Needed for DaVinci Resolv installer 
+    nvidia \
+    nvidia-opencl \
+    libopencv \
+    xcb-util  \
+    xcb-util-wm \
+    xcb-util-image \
+    xcb-util-keysyms \
+    xcb-util-renderutil \
+    libxkbcommon-x11 \
+    glu \
+#    qt-devel
+#    qt
+#    Qt
+    # libglvnd-devel
+    
+
+# My things, no relation to DaVinci installation
+xbps-install \
+    virtualbox-ose \
+    transmission \
+    cmake \
+    python3-devel \
+    fd 
+
+# Keyboard layout toggling for multilingual users
+cat <<'EOI' >> /etc/X11/xorg.conf.d/30-keyboard.conf
+Section "InputClass"
+	Identifier "keyboard"
+	MatchIsKeyboard "on"
+	Option "XKbLayout" "us,de,ru"
+	Option "XKbOptions" "grp:win_space_toggle"
+EndSection
+EOI
