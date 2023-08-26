e300fa9f79febf573afcd97277dc642c32a76692
Tommy Atkins <foo.bar@example.com>
2023-08-26 09:54:18 +0200

    Add usage and run installer

--- davinci_on_void_install.sh
+++ davinci_on_void_install.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+usage(){
+    cat <<EOU
+    Installs Davinci Resolve 18.5.1 
+    and dependencies on Void Linux / dwm desktop.
+    Must be run with root rights.
+
+    $0 <path/to/unzipped/davinci/installer>
+
+    This script serves my personal needs/preferences.
+    As these might be different from yours, 
+    read the code and edit before running it.
+
+EOU
+}
+
+DAVINCI_INSTALLER="$1"
+if [ ! -x "$DAVINCI_INSTALLER" ]; then
+    echo "Expecting path to Da Vinci installer"
+    usage
+fi
+
 # Nvidia drivers come from here
 xbps-install void-repo-nonfree xtools
 
@@ -12,7 +33,7 @@ xbps-install vim-huge git fzf fd tmux bash-completion lynx \
     google-fonts-ttf \
     xkblayout-state \
 
-    # Needed for DaVinci Resolv installer 
+    # DaVinci Resolve dependencies 
     nvidia \
     nvidia-opencl \
     libopencv \
@@ -22,20 +43,39 @@ xbps-install vim-huge git fzf fd tmux bash-completion lynx \
     xcb-util-keysyms \
     xcb-util-renderutil \
     libxkbcommon-x11 \
-    glu \
+    glu 
 #    qt-devel
 #    qt
 #    Qt
     # libglvnd-devel
-    
 
-# My things, no relation to DaVinci installation
+$DAVINCI_INSTALLER
+    
+# These point to outdated libs, 
+# but the newer ones are already installed.
+# So, just move them out of the way.
+mkdir /opt/resolve/libs/_disabled
+mv libgio-2.0.so* _disabled/
+mv libglib-2.0.so* _disabled/
+mv libgmodule-2.0.so* _disabled/
+mv libgobject-2.0.so* _disabled/
+
+# My things, no relation to DaVinci installation.
+# Use as a template or ditch altogether.
 xbps-install \
     virtualbox-ose \
     transmission \
     cmake \
     python3-devel \
-    fd 
+    fd \
+    feh
+
+# My ALSA config, system-specific.
+# See https://docs.voidlinux.org/config/media/alsa.html
+cat <<EOI >> /etc/alsa.conf
+defaults.ctl.card 1;
+defaults.pcm.card 1;
+EOI
 
 # Keyboard layout toggling for multilingual users
 cat <<'EOI' >> /etc/X11/xorg.conf.d/30-keyboard.conf
