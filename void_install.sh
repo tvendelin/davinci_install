41a651dd6165901a69412a58204b8a102e685839
Toomas Vendelin <toomas.vendelin@gmail.com>
2023-08-28 17:18:37 +0200

    Fix void_install.sh name

---
+++ void_install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+sudo xbps-install -u xbps
+
+# Nvidia drivers come from here
+sudo xbps-install -y void-repo-nonfree xtools
+
+# update everything
+sudo xbps-install -y -Su
+
+sudo xbps-install -y vim-huge git fzf fd tmux bash-completion lynx \
+    xorg alsa-utils \
+    base-devel libX11-devel libXft-devel libXinerama-devel \
+    google-fonts-ttf \
+    xkblayout-state \
+
+    # DaVinci Resolve dependencies
+    nvidia \
+    nvidia-opencl \
+    libopencv \
+    xcb-util  \
+    xcb-util-wm \
+    xcb-util-image \
+    xcb-util-keysyms \
+    xcb-util-renderutil \
+    libxkbcommon-x11 \
+    glu
+#    qt-devel
+#    qt
+#    Qt
+    # libglvnd-devel
+
+# Install Suckless software,
+# customize later
+mkdir $HOME/suckless && cd $HOME/suckless
+
+for R in st dwm dmenu slstatus; do
+    git clone "https://git.suckless.org/$R"
+    cd $R
+    sudo make clean install
+    cd ..
+done
+
+cd $HOME
+
+cat <<EOI > $HOME/.xinitrc
+/usr/local/bin/slstatus &
+exec dwm
+EOI
+
